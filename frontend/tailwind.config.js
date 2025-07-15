d10acf55a1f5d8d7a1da0a63c6ef2d4b6a6d4c8b
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-15 12:04:45 +0600

    add some custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4fbf8b',
+        secondary: '#44ae7c',
+      }
+    },
   },
   plugins: [],
 }
