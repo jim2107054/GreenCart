6191af0c21ef0f8dea2ce52efbf5671dc01d178c
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-15 12:28:50 +0600

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,11 +15,11 @@ const Navbar = () => {
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to="/" onClick={()=> scrollTo(0,0)}>
+            <NavLink to="/" onClick={()=> {scrollTo(0,0); setOpen(false)}}>
                 <img className='h-9' src={assets.logo} alt="Logo" />
             </NavLink>
 
-            {/* Desktop Menu */}
+            {/*---------------------Desktop Menu--------------------*/}
             <div className="hidden sm:flex items-center gap-8">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/products">All Products</NavLink>
@@ -30,18 +30,40 @@ const Navbar = () => {
                     <img className='w-4 h-4' src={assets.search_icon} alt="search" />
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div 
+                onClick={() => navigate('/cart')}
+                className="relative cursor-pointer">
                     <img className='w-6 opacity-80' src={assets.cart_icon} alt="cart" />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button 
+                {
+                    user ?
+                    (
+                        <div className='relative group'>
+                            <img
+                            className='w-10 h-10 rounded-full cursor-pointer'
+                             src={assets.profile_icon} alt="profile" />
+                            <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-40
+                             rounded-md text-sm z-40'>
+                                <li className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>
+                                    <NavLink to="/my-orders">My Orders</NavLink>
+                                </li>
+                                <li className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>
+                                    <button onClick={() => logout()}>Logout</button>
+                                </li>
+                            </ul>
+                        </div>
+                    ):(
+                        <button 
                 onClick={() => {
                     setShowUserLogin(true);
                 }}
                 className="cursor-pointer px-8 py-2 bg-primary hover:bg-secondary transition text-white rounded-full">
                     Login
                 </button>
+                    )
+                }
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -49,7 +71,7 @@ const Navbar = () => {
                 <img src={assets.menu_icon} alt="Menu" />
             </button>
 
-            {/* Mobile Menu */}
+            {/*---------------------Mobile Menu--------------------*/}
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to="/" onClick={()=>setOpen(false)}>Home</NavLink>
                 <NavLink to="/products" onClick={()=>setOpen(false)}>All Products</NavLink>
