179d4f3ddbc58e1911f4462a98522f43e90c8c98
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-17 23:31:06 +0600

    create product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
 const ProductCard = ({product}) => {
-  const [count, setCount] = useState(0);
-  const {currency} = useAppContext()
+  const {currency, cartItems, addToCart, updateCartItem, removeFromCart, navigate} = useAppContext()
 
-    return (
-        <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
+    return product && (
+        <div className="border hover:-translate-y-1 hover:scale-105 hover:shadow-md hover:shadow-gray-800/80 border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
             <div className="group cursor-pointer flex items-center justify-center px-2">
                 <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt={product.name} />
             </div>
@@ -21,24 +19,24 @@ const ProductCard = ({product}) => {
                     <p>({4})</p>
                 </div>
                 <div className="flex items-end justify-between mt-3">
-                    <p className="md:text-xl text-base font-medium text-indigo-500">
+                    <p className="md:text-xl text-base font-medium text-primary">
                         {currency}{product.offerPrice}{" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}{product.price}</span>
                     </p>
-                    <div className="text-indigo-500">
-                        {count === 0 ? (
-                            <button className="flex items-center justify-center gap-1 bg-indigo-100 border border-indigo-300 md:w-[80px] w-[64px] h-[34px] rounded text-indigo-600 font-medium" onClick={() => setCount(1)} >
-                                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
+                    <div 
+                    onClick={(e) => e.stopPropagation()}
+                    className="text-primary">
+                        {!cartItems[product._id] ? (
+                            <button className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded text-primary-600 font-medium" onClick={() => addToCart(product._id)} >
+                                <img className="md:w-5 w-4" src={assets.cart_icon} alt="cart_icon" />
                                 Add
                             </button>
                         ) : (
-                            <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-indigo-500/25 rounded select-none">
-                                <button onClick={() => setCount((prev) => Math.max(prev - 1, 0))} className="cursor-pointer text-md px-2 h-full" >
+                            <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-primary/25 rounded select-none">
+                                <button onClick={() => removeFromCart(product._id)} className="cursor-pointer text-md px-2 h-full" >
                                     -
                                 </button>
-                                <span className="w-5 text-center">{count}</span>
-                                <button onClick={() => setCount((prev) => prev + 1)} className="cursor-pointer text-md px-2 h-full" >
+                                <span className="w-5 text-center">{cartItems[product._id]}</span>
+                                <button onClick={() => addToCart(product._id)} className="cursor-pointer text-md px-2 h-full" >
                                     +
                                 </button>
                             </div>
