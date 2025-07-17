67986c1ff3bbbaeb29d19f5e44b76244ccfdc1b6
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-17 23:30:49 +0600

    create best seller section

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -7,8 +7,8 @@ const BestSeller = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div>
-        {products.map((product) => (
+      <div className="mt-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6">
+        {products.filter((product) => product.inStock).slice(0,5).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
