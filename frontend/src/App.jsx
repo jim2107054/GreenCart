0231bb01778a0f3cc322a3ea9adca53f2f454883
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-18 18:22:08 +0600

    add toast notifications styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
   // we are in the seller dashboard or not. if the path is /seller, we are in the seller dashboard
@@ -11,6 +12,39 @@ const App = () => {
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            background: "#fff",
+            color: "#000",
+            fontSize: "14px",
+          },
+          success: {
+            duration: 3000,
+            style: {
+              background: "#4caf50",
+              color: "#fff",
+            },
+          },
+          error: {
+            duration: 3000,
+            style: {
+              background: "#f44336",
+              color: "#fff",
+            },
+          },
+          loading: {
+            duration: 3000,
+            style: {
+              background: "#2196f3",
+              color: "#fff",
+            },
+          },
+        }}
+      />
+      
       <div
         className={` ${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
