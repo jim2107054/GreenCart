e659d931b6e3dce2d48097cf0bb723be3646294b
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-19 18:46:48 +0600

    search query state variable

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,72 +1,88 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyProducts } from './../assets/assets';
+import { dummyProducts } from "./../assets/assets";
 import { toast } from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.env.VITE_CURRENCY;
+  const navigate = useNavigate();
+  const [user, setUser] = useState(false);
+  const [isSaller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
 
+  // Fetch products from an API or use dummy data
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
 
-    const currency = import.meta.env.VITE_CURRENCY
-    const navigate = useNavigate();
-    const [user, setUser] = useState(false)
-    const [isSaller, setIsSeller] = useState(false);
-    const [showUserLogin, setShowUserLogin] = useState(false);
-    const [products, setProducts] = useState([]);
-    const [cartItems, setCartItems] = useState({});
-
-    // Fetch products from an API or use dummy data
-    const fetchProducts = async () => {
-        setProducts(dummyProducts)
+  // Add products to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
     }
+    setCartItems(cartData);
+    toast.success("Added to cart");
+  };
 
-    // Add products to cart
-    const addToCart = (itemId) => {
-        let cartData = structuredClone(cartItems);
-        if(cartData[itemId]){
-            cartData[itemId] += 1;
-        }
-        else {
-            cartData[itemId] = 1;
-        }
-        setCartItems(cartData);
-        toast.success("Added to cart");
+  // update cart items quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart updated");
+  };
+
+  // Remove product from cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
     }
+    setCartItems(cartData);
+    toast.success("Removed from cart");
+  };
 
-    // update cart items quantity
-    const updateCartItem = (itemId, quantity) => {
-        let cartData = structuredClone(cartItems)
-        cartData[itemId] = quantity;
-        setCartItems(cartData);
-        toast.success("Cart updated");
-    }
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    // Remove product from cart
-    const removeFromCart = (itemId) => {
-        let cartData = structuredClone(cartItems);
-        if(cartData[itemId]){
-            cartData[itemId] -= 1;
-            if(cartData[itemId] === 0){
-                delete cartData[itemId];
-            }
-        }
-        setCartItems(cartData);
-        toast.success("Removed from cart");
-    }
-
-    useEffect(()=> {
-        fetchProducts();
-    }, []);
-
-    const value = { navigate, user, setUser, isSaller, setIsSeller, showUserLogin, setShowUserLogin, products, setProducts, currency, cartItems, setCartItems, addToCart, updateCartItem, removeFromCart };
-    return (
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSaller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    setProducts,
+    currency,
+    cartItems,
+    setCartItems,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    searchQuery,
+    setSearchQuery,
+  };
+  return (
         <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
     )
-}
+};
 
 export const useAppContext = () => {
-    return useContext(AppContext);
-}
+  return useContext(AppContext);
+};
