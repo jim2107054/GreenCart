bd216d030bc341020cc081b08d4e4e603afc3c1d
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-19 18:46:21 +0600

    add all products route and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import {Toaster} from "react-hot-toast";
 import Footer from "./components/Footer";
 import { useAppContext } from "./context/AppContext";
 import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
 
 const App = () => {
   // we are in the seller dashboard or not. if the path is /seller, we are in the seller dashboard
@@ -58,6 +59,7 @@ const App = () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
