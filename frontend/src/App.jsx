59ac863f7229095c8171bebfe1a95b88989d7d0a
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-19 20:42:01 +0600

    show products based on their category

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import { useAppContext } from "./context/AppContext";
 import Login from "./components/Login";
 import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
 
 const App = () => {
   // we are in the seller dashboard or not. if the path is /seller, we are in the seller dashboard
@@ -60,6 +61,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
