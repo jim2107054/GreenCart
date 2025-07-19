fba55b714a680c6f506a00eb4ab09fd3c9ff42cb
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-19 18:47:09 +0600

    create all products page

---
+++ frontend/src/pages/AllProducts.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import ProductCard from "../components/ProductCard";
+
+const AllProducts = () => {
+    const {products, searchQuery} = useAppContext();
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    // Filter products based on search query
+    useEffect(()=> {
+        if(searchQuery.length>0){
+            const filtered = products.filter(product => 
+                product.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+            )
+            setFilteredProducts(filtered);
+        }
+        else{
+            setFilteredProducts(products);
+        }
+    },[searchQuery, products]);
+  return (
+    <div className="mt-16 flex flex-col">
+        <div className="flex flex-col items-end w-max">
+            <p className="text-2xl font-medium uppercase">All Products</p>
+            <div className="w-16 h-0.5 bg-primary rounded-full"/>
+        </div>
+        {/*----------------Display products------------*/}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-8">
+            {
+                filteredProducts.filter((product)=> product.inStock).map((item,index)=>(
+                    <ProductCard key={index} product={item} />
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default AllProducts
