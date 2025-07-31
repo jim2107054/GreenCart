df7e0c9bd014398731d19e5d66a42352965e3d22
jim2107054 <mdjahid205hasan@gmail.com>
2025-07-31 06:20:21 +0600

    start the server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-console.log("hello")
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const allowedOrigins = [
+    'http://localhost:5173'
+]
+
+// Middleware congfiguration
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cookieParser()); // Middleware to parse cookies
+app.use(cors({
+    origin: allowedOrigins, // Replace with your allowed origins
+    credentials: true // Allow credentials to be sent
+}))
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to GreenCart Backend');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
