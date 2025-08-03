4a192a30754a58954b2aeb2e9e6fcdcf518e2903
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-03 09:07:53 +0600

    create addProduct controller function

---
+++ backend/controllers/productController.js
@@ -0,0 +1,56 @@
+import {v2 as cloudinary} from "cloudinary";
+import Product from "../models/product.js";
+
+// Add Product : /api/product/add
+export const addProduct = async (req, res) =>{
+    try {
+        let productData = JSON.parse(req.body.productData)
+
+        const images = req.files
+
+        // let imageUrl = [];
+        // for (let i = 0; i < images.length; i++) {
+        //     const result = await cloudinary.uploader.upload(images[i].path, {
+        //         folder: "products",
+        //     });
+        //     imageUrl.push({
+        //         public_id: result.public_id,
+        //         url: result.secure_url,
+        //     });
+        // }
+
+        let imageUrl = await Promise.all(
+            images.map( async (image) => {
+                let result = await cloudinary.uploader.upload(image.path, {
+                    resource_type: "image"
+                });
+                return result.secure_url;
+            })
+        )
+
+        await Product.create({
+            ...productData,
+            image: imageUrl
+        });
+
+        res.json({ success: true, message: "Product added" });
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+// Get All Products : /api/product/list
+export const productList = async (req, res) =>{
+
+}
+
+// Get Product by ID : /api/product/:id
+export const productById = async (req, res) => {
+}
+
+// Change Product inStock : /api/product/stock
+export const changeStock = async (req, res) => {
+    
+}
